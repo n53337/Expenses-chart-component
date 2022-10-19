af236a1516bc16e41edd74df3d60bb583264f12c
u53f <scalewithusef@gmail.com>
2022-10-19 10:37:20 +0100

    using json to display data

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,44 @@
 "use srtict";
+
+// Elements
+const statsChart = document.querySelectorAll(".stats__vector-item");
+const statsValueHolder = document.querySelectorAll(".stats__vector-data");
+const days = document.querySelectorAll(".stats__day");
+
+// Get JSON file
+
+const getData = async () => {
+  try {
+    const req = await fetch("/data.json");
+    const data = await req.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Render Data to chart
+
+const renderChart = async () => {
+  const data = await getData();
+  statsValueHolder.forEach((e, i) => {
+    // render amount and day
+    e.textContent = `$${data[i].amount}`;
+    days[i].textContent = data[i].day;
+
+    // dynamic chart width using data
+  });
+};
+renderChart();
+
+// Show stats
+
+statsChart.forEach((e, i) => {
+  e.addEventListener("mouseenter", () => {
+    statsValueHolder[i].classList.toggle("d-none");
+  });
+
+  e.addEventListener("mouseout", () => {
+    statsValueHolder[i].classList.toggle("d-none");
+  });
+});
