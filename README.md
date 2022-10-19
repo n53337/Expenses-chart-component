a64cb709be0501ce1ae1266a9bbdf8b2f8c736f8
u53f <scalewithusef@gmail.com>
2022-10-19 12:31:13 +0100

    README

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Expenses-chart-component
+# Frontend Mentor - Interactive card details form solution
+
+This is a solution to the [Interactive card details form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day's bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Bonus: See dynamically generated bars based on the data provided in the local JSON file
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Live Site URL: [click here!](https://fem-cards.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5
+- CSS
+- Flexbox
+- CSS Grid
+- Javascript (Async/Await)
+
+### What I learned
+
+i really enjoy making this mini app, i've practiced my skills in Async Javascript
+
+## Author
+
+- github - [u53f](https://github.com/n53337)
+- Frontend Mentor - [@n53337](https://www.frontendmentor.io/profile/n53337)
+- Twitter - [@n_53337](https://twitter.com/n_53337)
--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,53 @@
 "use srtict";
 
-// Elements
-const statsChart = document.querySelectorAll(".stats__vector-item");
-const statsValueHolder = document.querySelectorAll(".stats__vector-data");
-const days = document.querySelectorAll(".stats__day");
+const App = () => {
+  // Elements
+  const statsChart = document.querySelectorAll(".stats__vector-item");
+  const statsValueHolder = document.querySelectorAll(".stats__vector-data");
+  const days = document.querySelectorAll(".stats__day");
 
-// Get JSON file
-const getData = async () => {
-  try {
-    const req = await fetch("/data.json");
-    const data = await req.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
+  // Get JSON file
+  const getData = async () => {
+    try {
+      const req = await fetch("/data.json");
+      const data = await req.json();
+      return data;
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-// Render chart using JSON data
-const renderChart = async () => {
-  const data = await getData();
+  // Render chart using JSON data
+  const renderChart = async () => {
+    const data = await getData();
+    statsChart.forEach((e, i) => {
+      // render amount and day
+      statsValueHolder[i].textContent = `$${data[i].amount}`;
+      days[i].textContent = data[i].day;
+
+      // dynamic chart bar width using data
+      e.style.height = `${data[i].amount / 5}rem`;
+    });
+
+    // highest amount color change
+    const amounts = data.map((e) => e.amount);
+    const max = Math.max(...amounts);
+    const index = amounts.indexOf(max);
+    statsChart[index].style.background = "var(--clr-cyan)";
+  };
+  renderChart();
+
+  // Show stats
+
   statsChart.forEach((e, i) => {
-    // render amount and day
-    statsValueHolder[i].textContent = `$${data[i].amount}`;
-    days[i].textContent = data[i].day;
+    e.addEventListener("mouseenter", () => {
+      statsValueHolder[i].classList.toggle("d-none");
+    });
 
-    // dynamic chart bar width using data
-    e.style.height = `${data[i].amount / 5}rem`;
+    e.addEventListener("mouseout", () => {
+      statsValueHolder[i].classList.toggle("d-none");
+    });
   });
-
-  // highest amount color change
-  const amounts = data.map((e) => e.amount);
-  const max = Math.max(...amounts);
-  const index = amounts.indexOf(max);
-  console.log(amounts, index);
-  statsChart[index].style.background = "var(--clr-cyan)";
 };
-renderChart();
 
-// Show stats
-
-statsChart.forEach((e, i) => {
-  e.addEventListener("mouseenter", () => {
-    statsValueHolder[i].classList.toggle("d-none");
-  });
-
-  e.addEventListener("mouseout", () => {
-    statsValueHolder[i].classList.toggle("d-none");
-  });
-});
+window.addEventListener("load", App);
